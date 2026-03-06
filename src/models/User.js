cb8b2c0889e37b92c8d4f85f86602b07ce4f1eb0
shamsjubair111 <shamsjubair.sj@gmail.com>
2026-03-06 14:35:05 +0600

    user model changed

--- src/models/User.js
+++ src/models/User.js
@@ -4,14 +4,16 @@ const bcrypt = require('bcryptjs');
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String },
+  googleId: { type: String },
+  avatar: { type: String },
   role: { type: String, enum: ['user','author','admin'], default: 'user' },
   createdAt: { type: Date, default: Date.now }
 });
 
 // Hash password before saving
 UserSchema.pre('save', async function(next){
-  if(!this.isModified('password')) return next();
+  if(!this.password || !this.isModified('password')) return next();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
